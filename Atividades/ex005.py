f71b65b6fdc7d689e94e7d44eb038083c87ea29c
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-05-13 16:44:22 -0300

    update atualização

--- Atividades/ex005.py
+++ Atividades/ex005.py
@@ -1,4 +1,5 @@
 n = int(input('digite um numero: '))
 a = n - 1
 s = n + 1
-print(f"Você digitou {n} o numero antecessor a esse é {a} e o seu sucessor é {s}")
+#print("Você digit {} o numero antecessor a esse é {} e o seu sucessor é {}".format(n, (n-1), (n+1)))
+print(f'Analisando o valor {n}, seu antecessor é {a} e o sucessor é {s}')
