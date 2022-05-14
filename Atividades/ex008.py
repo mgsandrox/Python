c37a0a26170f92e82d7fa46b171a2b1f43f62af4
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-05-13 21:27:21 -0300

    Conversor de Medidas

---
+++ Atividades/ex008.py
@@ -0,0 +1,2 @@
+d = float(input('A quantos metros você está: '))
+print(f'{d} metros tem: \n {d * 100}cm \n {d * 1000:.01f}mm \n {d * 10:.01f}dm \n {d / 10:.01f}dam \n {d / 100:.02f}hm \n {d / 1000:.02f}km')
