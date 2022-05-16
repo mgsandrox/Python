3b20f789269ac1cd2d2c0699b423def6056e0bbd
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-05-16 16:39:42 -0300

    Update ex14.py

--- Atividades/ex14.py
+++ Atividades/ex14.py
@@ -6,5 +6,5 @@ k1 = (((f - 32) * 5) / 9) + 273
 f1 = ((k - 273) * 1.8) + 32
 c2 = k - 273
 print(f' A conversão de {c}°C para fahrenheit e de {f}°F e em kelvin é {k}°K')
-print(f'A conversão de {f}°F para celcius {c1}°C  e de kelvin é {k1}°K ')
-print(f'A conversão de {k}°K para fahrenheit {f1}°F e  para celsius {c2}°C')
+#print(f'A conversão de {f}°F para celcius {c1}°C  e de kelvin é {k1}°K ')
+#print(f'A conversão de {k}°K para fahrenheit {f1}°F e  para celsius {c2}°C')
