17caf07cb68c5360789495344c4a02d310b4f4d9
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-05-16 15:57:20 -0300

    update ex12

--- Atividades/ex12.py
+++ Atividades/ex12.py
@@ -2,5 +2,5 @@ prod = float(input('Qual o valor do produto? '))
 desc = float(input('Qual o valor do desconto desejado? '))
 desconto = desc * prod / 100
 promocao = prod - desconto
-
-print(f'O valor do desconto foi de {desconto:.2f} você irá levar o produto por {promocao:.2f}!')
+parcelado = prod + (prod * desc / 100)
+print(f'O valor do desconto foi de {desconto:.2f} você irá levar o produto a vista por {promocao:.2f} e se parcelar sairar por {parcelado}')
