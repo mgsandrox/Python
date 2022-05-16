d91926c068fbbe8e2e64a6d764b77eee1c6a337f
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-05-16 13:49:06 -0300

    calculo de pintura de parede

---
+++ Atividades/ex11.py
@@ -0,0 +1,8 @@
+larg = float(input('Insira a largura da parede: '))
+altu = float(input('Insire a altura da parede: '))
+area = larg * altu
+tinta =  area / 2
+print(f'Sua parede tem a dimensão de {larg}x{altu} correspondendo a uma área de {area:.2f}m²')
+print(f'Você precisara de {tinta:.2f}L para pintar essa parede!')
+
+
