e9d255b91987d037c09a78f06ffb2e22259b0030
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-07-06 21:09:36 -0300

    lista randômica

---
+++ Atividades/ex19.py
@@ -0,0 +1,9 @@
+from random import choice
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+n5 = str(input('Quinto aluno: '))
+lista = [n1, n2, n3, n4, n5]
+escolhido = choice(lista)
+print(f'O aluno escolhido foi {escolhido}!!!')
