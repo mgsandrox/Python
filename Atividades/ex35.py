d618ae4ee98521ea729d6e1c0a0d5c6ebbc289a6
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-07-21 21:58:26 -0300

    .

--- Atividades/ex35.py
+++ Atividades/ex35.py
@@ -1,8 +1,6 @@
 a = int(input('Digite um valor: '))
 b = int(input('Digite outro valor: '))
 c = int(input('Digite mais um valor: '))
-
-
 if (a + b>= c) and (a + c >= b) and (b + c >= a):
     print('Esse triangulo é valido!')
 else:
