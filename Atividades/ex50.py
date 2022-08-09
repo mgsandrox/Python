0aa6434340c90d912f26a4fc2f6972dd64b7108a
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-08-09 12:10:58 -0300

    mudanças ex50

--- Atividades/ex50.py
+++ Atividades/ex50.py
@@ -1,11 +1,14 @@
+from time import sleep
 soma = 0
 cont = 0
+print('Informe alguns números eles serão somado!!!')
+sleep(1)
 for c in range(1, 7):
     n1 = int(input('Digite um número: '))
     if n1 % 2 == 0:
         soma = soma + n1
         cont += 1
-    elif n1 % 2 == 1:
+    elif n1 % 2 != 0:
         soma += n1
         cont += 1
 print(f'Foram informados {cont} números e a soma foi {soma}')
