5349df7fe5b544a98c04dc08b8c6476f4f588d4a
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-08-08 21:54:51 -0300

    upgrade ex46

--- Atividades/ex46.py
+++ Atividades/ex46.py
@@ -4,10 +4,7 @@ print('''
 [1] Vou acender os fogos!!!
 [2] Não vou acender!
 ''')
-
 cont = int(input('Qual a sua escolha: '))
-
-
 if cont == 1:
     for c in range(10,-1,-1):
         print(c)
@@ -15,10 +12,8 @@ if cont == 1:
     print('BUM BUMM POWW!')
     sleep(1)
     print('Fogos acesos!!')
-
 elif cont == 2:
     sleep(1)
     print('Ficou com medo né!!!')
-
 else:
     print('Opção invalida!!')
