0be701b71e0bbb3e125e17c6e369e9bf92855b62
Alessandro Tavares <80715104+mgsandrox@users.noreply.github.com>
2022-09-09 10:20:08 -0300

    Fatorial

--- Atividades/ex60.py
+++ Atividades/ex60.py
@@ -1,4 +1,3 @@
-
 #Fatorial do jeito simples
 '''from math import factorial
 n = int(input('Digitr um numero:'))
@@ -7,7 +6,7 @@ print(f'O fatorial de {n} e {f}')'''
 
 
 #Fatorial mais complicado
-n =  int(input('Informe um numero:'))
+'''n =  int(input('Informe um numero:'))
 c = n
 f = 1
 print(f'Calculando {n}! = ', end='')
@@ -16,4 +15,13 @@ while c > 0:
     print(f' x ' if c > 1 else ' = ', end='')
     f *= c
     c -= 1
-print(f'{f}')
+print(f'{f}')'''
+
+#Fatorial com FOR
+numero = int(input("Fatorial de: ") )
+
+resultado=1
+for n in range(1,numero+1):
+    resultado *= n
+
+print(resultado)
